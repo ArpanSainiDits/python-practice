500a84cf08bf6e109dcfa74d9d82073ae1b80b8b
arpan saini <arpansainiwins@gmail.com>
2021-11-30 09:41:30 +0530

    change urls

--- project2/api_basic/urls.py
+++ project2/api_basic/urls.py
@@ -7,24 +7,13 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('student', StudentViewSet, basename='student')
-
-router2 = DefaultRouter()
-router2.register('student', StudentGenericViewSet, basename= 'student')
-
-router3 = DefaultRouter()
-router3.register('student', StudentModelViewSet, basename='student')
+router.register('genericstudent', StudentGenericViewSet, basename= 'student')
+router.register('modelstudent', StudentModelViewSet, basename='student')
 
 
 urlpatterns = [
-    path('modelviewset/', include(router3.urls)),
-    path('modelviewset/<int:pk>/', include(router3.urls)),
-    
-    path('genericviewset/', include(router2.urls)),
-    path('genericviewset/<int:pk>/', include(router2.urls)),
-    
     path('viewset/',include(router.urls)),
     path('viewset/<int:pk>/', include(router.urls)),
-
     path('student/', student_list),
     path('student1/', student_list1),
     path('studentId/<int:pk>/', student_detail),
